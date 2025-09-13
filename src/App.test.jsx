715348a1fb62fdc03de3b8ac53afe3eb8d2a4d61
Patrick Ritter <patrick.ritter90@gmail.com>
2025-09-13 07:59:03 +0200

    fix es lint issue

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,5 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('App', () => {
